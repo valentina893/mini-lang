ec01b1a93ddd306b3d6de12847fba308a247f710
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-03-21 13:08:41 -0500

    removed: old variable_assignment parsing functions. added: type enum for each variable, value length and type fields in variable struct, new and cleaner functions for parsing assignment lines, parse_line(), parse_variable_value(), parse_variable_name(), existing_variable

--- src/mini.c
+++ src/mini.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,182 +6,132 @@
 #define max_vars 10
 #define buf_capacity 100
 
+typedef enum type {
+    integer,
+    string,
+    none
+} type;
+
 typedef struct variable {
     char *name, *value;
+    size_t len;
+    type _type;
 } variable;
 
-variable variable_arr[max_vars];
+// struct for line as well?
+
 int variable_amt = 0;
+FILE *fptr = NULL;
+char line[1024];
+variable variable_arr[max_vars];
+
+int existing_variable(char *name) {
+
+    for (int i = 0; i < variable_amt; i++) {
+        if (strcmp(name, variable_arr[i].name) == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+
+}
+
+int parse_variable_name(long assignment_pos, variable *variable) {
+
+    int reading = 0;
+
+    int start = 0;
+    int length = 1;
+
+    // read from start to position of '=' char
+    for (int i = 0; i < assignment_pos; i++) {
+
+        if (line[i] != ' ' && reading == 0) { // read first character
+            reading = 1;
+            start = i;
+        } else if (line[i] == ' ' && reading == 1) { // read whitespace after name
+            break;
+        } else if (line[i] != ' ' && reading == 1) { // read next character
+            length++;
+        }
+
+    }
+
+    variable->name = (char*)malloc(sizeof(char) * length);
+    strncpy(variable->name, line + start, length);
+
+    return 1;
+
+}
+
+int parse_variable_value(int line_len, long assignment_pos, variable *variable) {
+
+    int reading = 0;
+
+    int start = assignment_pos + 1;
+    int length = 1;
+
+    // read from position of '=' char to end of line
+    for (int i = assignment_pos + 1; i < line_len; i++) {
+
+        if (line[i] != ' ' && reading == 0) { // read first character
+            reading = 1;
+            start = i;
+        } else if ((line[i] == ' ' || line[i] == '\n' || line[i] == '\0') && reading == 1) { // read whitespace, newline, or null char after value
+            break;
+        } else if (line[i] != ' ' && reading == 1) { // read next character
+            length++;
+        }
+
+    }
+
+    variable->value = (char*)malloc(sizeof(char) * length);
+    variable->len = length;
+    strncpy(variable->value, line + start, length);
+
+    return 1;
+
+}
+
+void parse_line(int line_len) {
+
+    variable variable = {NULL, NULL, 0, none};
+
+    char *assignment = strchr(line, '=');
+
+    if (assignment != NULL) { // line contains assignment operator
+
+        long assignment_pos = assignment - line;
+
+        if (parse_variable_name(assignment_pos, &variable) != 0) {
+            if (parse_variable_value(line_len, assignment_pos, &variable) != 0) {
+
+                int existing_pos = existing_variable(variable.name); // check if overwriting
+
+                if (existing_pos < 0) { // new variable
+                    variable_arr[variable_amt] = variable;
+                    variable_amt++;
+                } else { // existing variable
+                    variable_arr[existing_pos].value = (char*)realloc(variable.value, variable.len);
+                    free(variable.name);
+                }
+
+            } else {
+                printf("error parsing value\n");
+            }
+        } else {
+            printf("error parsing name\n");
+        }
+        
+    }
+
+}
 
 void print_variables() {
 
     for (int i = 0; i < variable_amt; i++) {
         printf("%s = %s\n", variable_arr[i].name, variable_arr[i].value);
-    }
-
-}
-
-void variable_assignment_parse_name(long equals_pos, char *line, int *existing_var, variable *variable) {
-
-    char buf[buf_capacity];
-    int buf_size = 0;
-
-    int reading = 0;
-
-    for (int i = 0; i < equals_pos; i++) { // parse variable name
-
-        if (line[i] != ' ') {
-            if (reading == 0) reading = 1;
-                buf[buf_size] = line[i];
-                buf_size++;
-        } else {
-            if (reading == 1) {
-                buf[buf_size] = '\0';
-                reading = 0;
-                break;
-            }
-        }
-
-    }
-
-    variable->name = (char*)malloc(sizeof(char) * buf_size);
-    strcpy(variable->name, buf);
-
-    for (int i = 0; i < variable_amt; i++) { // check if variable already exists
-        if (strcmp(buf, variable_arr[i].name) == 0) {
-            *existing_var = i; // equals -1 if new, equals i to indicate existing position
-            break;
-        }
-    }
-
-}
-
-void variable_assignment_parse_operation(int line_len, long equals_pos, char *line, char *strchr_op, variable *variable) {
-
-    char first_buf[100], second_buf[100];
-    int first_size = 0, second_size = 0;
-
-    long op_pos = strchr_op - line;
-
-    int reading = 0;
-
-    for (int i = equals_pos + 1; i < op_pos; i++) { // parse first operand
-
-        if (line[i] != ' ') {
-            if (reading == 0) reading = 1;
-                first_buf[first_size] = line[i];
-                first_size++;
-        } else {
-            if (reading == 1) {
-                first_buf[first_size] = '\0';
-                reading = 0;
-                break;
-            }
-        }
-
-    }
-
-    for (int i = op_pos + 1; i < line_len; i++) { // parse second operand
-
-        if (line[i] != ' ' && line[i] != '\n' && line[i] != '\0') {
-            if (reading == 0) reading = 1;
-                second_buf[second_size] = line[i];
-                second_size++;
-        } else {
-            if (reading == 1) {
-                second_buf[second_size] = '\0';
-                reading = 0;
-                break;
-            }
-        }
-
-    }
-
-    variable->value = (char*)malloc(sizeof(char) * buf_capacity);
-
-    int op_res = 0;
-    if (line[op_pos] == '+') {
-        op_res = atoi(first_buf) + atoi(second_buf);
-    }
-
-    sprintf(variable->value, "%d", op_res);
-
-}
-
-void variable_assignment_parse_value(int line_len, long equals_pos, char *line, variable *variable) {
-
-    char buf[buf_capacity];
-    int buf_size = 0;
-
-    int reading = 0;
-    int variable_on_variable = 0;
-
-    for (int i = equals_pos + 1; i < line_len; i++) { // parse single variable value
-
-        if (line[i] != ' ' && line[i] != '\n' && line[i] != '\0') {
-            if (reading == 0) reading = 1;
-                buf[buf_size] = line[i];
-                buf_size++;
-        } else {
-            if (reading == 1) {
-                buf[buf_size] = '\0';
-                reading = 0;
-                break;
-            }
-        }
-
-    }
-
-    // check if we are assigning variable to variable
-    for (int i = 0; i < variable_amt; i++) {
-        if (strcmp(buf, variable_arr[i].name) == 0) {
-            variable->value = (char*)malloc(sizeof(char) * strlen(variable_arr[i].value));
-            strcpy(variable->value, variable_arr[i].value);
-            variable_on_variable = 1;
-            break;
-        }
-    }
-
-    if (variable_on_variable == 0) {
-        variable->value = (char*)malloc(sizeof(char) * buf_size);
-        strcpy(variable->value, buf);
-    }
-
-
-}
-
-void variable_assignment(int line_len, char *line, char *strchr_res) {
-
-    variable variable = {NULL, NULL};
-    int existing_var = -1;
-
-    long equals_pos = strchr_res - line;
-
-    variable_assignment_parse_name(equals_pos, line, &existing_var, &variable);
-
-    char operations[] = {'+', '-', '*', '/'};
-    int operations_amt = 4;
-
-    char *strchr_op = NULL;
-
-    for (int i = 0; i < operations_amt; i++) {
-        strchr_op = strchr(line, operations[i]);
-        if (strchr_op != NULL) {
-            break;
-        }
-    }
-
-    if (strchr_op != NULL) {
-        variable_assignment_parse_operation(line_len, equals_pos, line, strchr_op, &variable);
-    } else {
-        variable_assignment_parse_value(line_len, equals_pos, line, &variable);
-    }
-
-    if (existing_var >= 0) {
-        variable_arr[existing_var].value = (char*)realloc(variable.value, strlen(variable.value));
-    } else {
-        variable_arr[variable_amt] = variable;
-        variable_amt++;
     }
 
 }
@@ -207,7 +158,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    FILE *fptr = fopen(argv[1], "r");
+    fptr = fopen(argv[1], "r");
 
     if (fptr == NULL) {
         printf("Unknown file: %s\n", argv[1]);
@@ -215,20 +166,12 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    char line[1024];
-
     while (fgets(line, 100, fptr)) {
         int line_len = strlen(line);
         if (line_len > 0) {
-            if (line[0] != '\n') {
+            if (line[0] != '\n') { // line isn't whitespace
 
-                char *strchr_res = strchr(line, '=');
-
-                if (strchr_res != NULL) { // line assigns value to a variable
-
-                    variable_assignment(line_len, line, strchr_res);
-
-                }
+                parse_line(line_len);
             
             }
         }
