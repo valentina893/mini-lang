b3d2e6f36ba9fbf7b343c900dc91d257f17d7114
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-05-06 14:45:52 -0500

    removed include statements, changed token_max macro to token_amt

--- src/main.c
+++ src/main.c
@@ -1,12 +1,6 @@
-#include <ctype.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "scanner.h"
 
-#define token_max 100
+#define token_amt 100
 
 int main(int argc, char **argv) {
 
@@ -15,7 +9,7 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    scanner *scanner = scanner_init(argv[1], token_max);
+    scanner *scanner = scanner_init(argv[1], token_amt);
     scanner_get_tokens(scanner);
     _scanner_print_tokens(scanner);
     scanner_delete(scanner);
