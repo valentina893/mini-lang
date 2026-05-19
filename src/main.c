30e53d964bc8830914f967ba4b9ed0ab36117f2b
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-05-18 22:31:44 -0500

    added read_args() helper

--- src/main.c
+++ src/main.c
@@ -3,22 +3,34 @@
 
 #define token_amt 100
 
-int main(int argc, char **argv) {
-
-    if (argc < 2) {
-        printf("Usage: ./mini <filename>\n");
+int read_args(int argc, char **argv, char *filename) {
+    if (argc != 2) {
+        printf("mini: no file specified\n");
         return 0;
     }
+    if (strstr(argv[1], ".mini") == NULL) {
+        printf("mini: file not of type .mini\n");
+        return 0;
+    }
+    size_t len = strlen(argv[1]);
+    strncpy(filename, argv[1], len);
+    filename[len] = '\0';
+    return 1;
+}
 
-    scanner *scanner = scanner_init(argv[1], token_amt);
-    scanner_get_tokens(scanner);
+int main(int argc, char **argv) {
 
-    evaluator *evaluator =  evaluator_init(scanner->tokens_amt, scanner->tokens);
-    evaluator_run(evaluator);
-
-    // must free evaluator
-
-    scanner_delete(scanner);
+    char filename[100];
+    if (read_args(argc, argv, filename) == 1) {
+        scanner *scanner = scanner_init(filename, token_amt);
+        if (scanner != NULL) {
+            scanner_get_tokens(scanner);
+            evaluator *evaluator =  evaluator_init(scanner->tokens_amt, scanner->tokens);
+            evaluator_run(evaluator);
+            // free evaluator
+            scanner_delete(scanner);
+        }
+    }
 
     return 0;
 }
