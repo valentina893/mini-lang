e3ae79d18c9d21850939820aafa593de9dcab453
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-05-21 23:01:47 -0500

    args class is initialized and passed to scanner

--- src/main.c
+++ src/main.c
@@ -1,28 +1,14 @@
+#include "args.h"
 #include "evaluator.h"
 #include "scanner.h"
 
 #define token_amt 100
 
-int read_args(int argc, char **argv, char *filename) {
-    if (argc != 2) {
-        printf("mini: no file specified\n");
-        return 0;
-    }
-    if (strstr(argv[1], ".mini") == NULL) {
-        printf("mini: file '%s' not of type .mini\n", argv[1]);
-        return 0;
-    }
-    size_t len = strlen(argv[1]);
-    strncpy(filename, argv[1], len);
-    filename[len] = '\0';
-    return 1;
-}
-
 int main(int argc, char **argv) {
 
-    char filename[100];
-    if (read_args(argc, argv, filename) == 1) {
-        scanner *scanner = scanner_init(filename, token_amt);
+    args *args = args_init(argc, argv);
+    if (args != NULL) {
+        scanner *scanner = scanner_init(args, token_amt);
         if (scanner != NULL) {
             scanner_get_tokens(scanner);
             evaluator *evaluator =  evaluator_init(scanner->tokens_amt, scanner->tokens);
