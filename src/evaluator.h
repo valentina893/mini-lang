a6ac3f03677d643269ad30cff8e49c018b6df907
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-05-27 14:23:14 -0500

    shrunk src code to call more handler functions from variable class

--- src/evaluator.h
+++ src/evaluator.h
@@ -11,7 +11,7 @@ Evaluator class that reads tokens and uses stack-based approach to evaluate code
 */
 typedef struct evaluator {
     int tokens_amt, variable_amt;
-    token_stack *stack;
+    value_stack *stack;
     token **tokens;
     variable **memory;
 } evaluator;
@@ -37,158 +37,23 @@ Checks memory array for variable with same id and returns pointer to variable. R
 variable *_evaluator_variable_seen(evaluator *evaluator, char *id);
 
 /*
+Handles operand token by pushing appropriate token -> variable conversion to the stack.
+*/
+int _evaluator_handle_operand(evaluator *evaluator, token *curr_token);
+
+/*
 Handles operator token.
 */
 int _evaluator_handle_operator(evaluator *evaluator, token *curr_token);
 
 /*
-Handles assignment operators.
+Evaluates binary operation such as a = b, a == b, a + b, etc.
 */
-int _evaluator_handle_equal(evaluator *evaluator);
+int _evaluator_handle_binary_operation(evaluator *evaluator, token *curr_token);
 
 /*
-Handles assignment to a defined variable.
+Evaluates function calls.
 */
-int _evaluator_handle_equal_existing(evaluator *evaluator, variable *dest_var, token *src);
-
-/*
-Handles assignment to a new variable.
-*/
-variable *_evaluator_handle_equal_new(evaluator *evaluator, token *dest, token *src);
-
-/*
-Handles boolean operator == for equality.
-*/
-int _evaluator_handle_equal_equal(evaluator *evaluator);
-
-/*
-Handles boolean operator == for equality where first operand is a variable.
-*/
-int _evaluator_handle_equal_equal_variable(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles boolean operator == between integer variable and another variable.
-*/
-int _evaluator_handle_equal_equal_variable_int_variable(evaluator *evaluator, variable *a_var, token *a, token *b);
-
-/*
-Handles boolean operator == between integer variable and a primitive.
-*/
-int _evaluator_handle_equal_equal_variable_int_primitive(evaluator *evaluator, variable *a_var, token *a, token *b);
-
-/*
-Handles boolean operator == between string variable and another variable.
-*/
-int _evaluator_handle_equal_equal_variable_str_variable(evaluator *evaluator, variable *a_var, token *a, token *b);
-
-/*
-Handles boolean operator == between string variable and a primitive.
-*/
-int _evaluator_handle_equal_equal_variable_str_primitive(evaluator *evaluator, variable *a_var, token *a, token *b);
-
-/*
-Handles boolean operator == where first operand is an integer.
-*/
-int _evaluator_handle_equal_equal_integer(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles boolean operator == between integer and another variable.
-*/
-int _evaluator_handle_equal_equal_integer_variable(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles boolean operator == between integer and a primitive.
-*/
-int _evaluator_handle_equal_equal_integer_primitive(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles boolean operator == where first operand is an string.
-*/
-int _evaluator_handle_equal_equal_string(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles boolean operator == between string and another variable.
-*/
-int _evaluator_handle_equal_equal_string_variable(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles boolean operator == between string and a primitive.
-*/
-int _evaluator_handle_equal_equal_string_primitive(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles boolean operator != for inequality.
-*/
-int _evaluator_handle_not_equal(evaluator *evaluator);
-
-/*
-Handles plus operation, checks if being used arithmetically or for string concatenation.
-*/
-int _evaluator_handle_plus(evaluator *evaluator);
-
-/*
-Handles plus operation for variables.
-*/
-int _evaluator_handle_plus_variable(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles plus operation for operands having an integer variable and another variable.
-*/
-int _evaluator_handle_plus_variable_int_variable(evaluator *evaluator, variable *a_var, token *a, token *b);
-
-/*
-Handles plus operation for operands having an integer variable and a primitive.
-*/
-int _evaluator_handle_plus_variable_int_primitive(evaluator *evaluator, variable *a_var, token *a, token *b);
-
-/*
-Handles plus operation for operands having a string variable and another variable.
-*/
-int _evaluator_handle_plus_variable_string_variable(evaluator *evaluator, variable *a_var, token *a, token *b);
-
-/*
-Handles plus operation for operands having a string variable and a primitive.
-*/
-int _evaluator_handle_plus_variable_string_primitive(evaluator *evaluator, variable *a_var, token *a, token *b);
-
-/*
-Handles plus operation for integers.
-*/
-int _evaluator_handle_plus_integer(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles plus operation when operands is an integer and variable.
-*/
-int _evaluator_handle_plus_integer_variable(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles plus operation when operands is an integer and primitive.
-*/
-int _evaluator_handle_plus_integer_primitive(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles plus operation for strings.
-*/
-int _evaluator_handle_plus_string(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles plus operation where operands are a string and variable.
-*/
-int _evaluator_handle_plus_string_variable(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles plus operation for strings.
-*/
-int _evaluator_handle_plus_string_primitive(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles plus operation for strings.
-*/
-int _evaluator_handle_plus_string(evaluator *evaluator, token *a, token *b);
-
-/*
-Handles function calls.
-*/
-int _evaluator_handle_function(evaluator *evaluator, token *func_token);
+int _evaluator_handle_function(evaluator *evaluator, token *curr_token);
 
 #endif
