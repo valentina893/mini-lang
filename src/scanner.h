5adfc498afb8665a5fea73db321a378492f33e3a
Valentina <zenit@MacBookAir.ht.home>
2026-09-09 20:04:38 -0500

    added instantiated stack type for scanner class to use with dedicated stack function prefixes, updated ptoken_stack paramters to be ststack types

--- src/scanner.h
+++ src/scanner.h
@@ -5,13 +5,13 @@
 
 #include <stdio.h>
 
-#include "args.h"
+#define STACK_T struct token*
+#define STACK_PREFIX ststack_
+#define STACK_NAME ststack
 #include "stack.h"
+
+#include "args.h"
 #include "token.h"
-
-typedef struct token* ptoken;
-
-stack_t(ptoken);
 
 /*
 Scans source code and "tokenizes" it.
@@ -62,12 +62,12 @@ void _scanner_postfix(scanner *scanner);
 /*
 Pops operators out of token stack and appends to postfix array until '(' is found.
 */
-void _scanner_find_left_parentheses(token ***result, ptoken_stack *token_stack, int *j);
+void _scanner_find_left_parentheses(token ***result, ststack *token_stack, int *j);
 
 /*
 Pops operators from token stack until current token has lower precedence than top. Current token is then pushed to stack.
 */
-void _scanner_handle_operator(scanner *scanner, token **result, ptoken_stack *token_stack, token* curr_token, int *j);
+void _scanner_handle_operator(scanner *scanner, token **result, ststack *token_stack, token* curr_token, int *j);
 
 /*
 Resizes tokens array to be double its current maximum capacity.
