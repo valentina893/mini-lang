ceb337682c39019084563c9a6698acdf810f5d7a
Valentina <zenit@MacBookAir.ht.home>
2026-09-10 23:01:14 -0500

    added null pointer check to return value of fgets() in value_input()

--- src/variable.c
+++ src/variable.c
@@ -236,7 +236,8 @@ void value_print(value *a) {
 
 value *value_input() {
     char buf[1024];
-    fgets(buf, sizeof(buf), stdin);
+    char *res = fgets(buf, sizeof(buf), stdin);
+    if (res == NULL) return NULL;
     buf[strcspn(buf, "\n")] = '\0';
     return value_init_string(strlen(buf), buf);
 }
